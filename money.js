f4b83ebb46e0177ab48c60bcf5f47476670e8002
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-02-17 10:13:26 +0600

    6th commit

---
+++ money.js
@@ -0,0 +1,54 @@
+// calculate button
+
+const button = document.getElementById('calculate-button').addEventListener('click', function () {
+
+    // income input value
+    const incomeInput = document.getElementById('income-input');
+    const IncomeInputValue = parseFloat(incomeInput.value);
+
+    // clear input
+    incomeInput.value = '';
+
+
+
+
+
+
+
+    // fodd input value
+    const foodInput = document.getElementById('food-input');
+    const foodInputValue = parseFloat(foodInput.value);
+
+    // clear input
+    foodInput.value = '';
+
+
+    // rent-input value
+    const rentInput = document.getElementById('rent-input');
+    const rentInputValue = parseFloat(rentInput.value);
+
+
+    // clear input
+    rentInput.value = '';
+
+
+    // clothes-input
+    const clothesInput = document.getElementById
+
+
+    // // expenses-amount total
+    // const expensesAmount = document.getElementById('expenses-amount');
+    // const expensesToatalAmout = parseFloat(expensesAmount.innerText);
+    // // expensesAmount.innerText = expensesToatalAmout;
+    // console.log(expensesToatalAmout.innerText);
+    // expensesAmount.innerText = foodInputValue;
+
+
+
+
+
+
+
+
+
+})
