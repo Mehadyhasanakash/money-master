38f09b939355b27d4d26e1e932b0936c8cb07dfd
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-02-17 20:31:14 +0600

    10th commit

--- money.js
+++ money.js
@@ -1,13 +1,13 @@
 // calculate button
 
-function getAmount(money){
+function getAmount(money) {
 
   // fodd input value
   const foodInput = document.getElementById(money);
   const foodInputValue = parseFloat(foodInput.value);
 
   // clear input
-  foodInput.value = '';
+  // foodInput.value = '';
   return foodInputValue;
 }
 
@@ -25,50 +25,25 @@ const button = document.getElementById('calculate-button').addEventListener('cli
 
 
 
-    // rent-input value
+  // rent-input value
 
-    const rentInputValue = getAmount('rent-input');
-
-
-    
-
-    // clothes-input
-    
-    const clothersInputValue = getAmount('clothes-input');
+  const rentInputValue = getAmount('rent-input');
 
 
 
-    // expenses-amount total
-    const expensesAmount = document.getElementById('expenses-amount');
-    const expensesToatalAmout = parseFloat(expensesAmount.innerText);
 
-    const totalAmount = expensesToatalAmout+ foodInputValue + rentInputValue +clothersInputValue ;
-    expensesAmount.innerText = totalAmount;
+  // clothes-input
 
-   
+  const clothersInputValue = getAmount('clothes-input');
 
 
-     
 
-    
-    // income input value
-    
-    const IncomeInputValue = getAmount('income-input');
-   
+  // expenses-amount total
+  const expensesAmount = document.getElementById('expenses-amount');
+  const expensesToatalAmout = parseFloat(expensesAmount.innerText);
 
-     
- 
-    
-    const totalBalance = document.getElementById('total-Balance');
-    const preBalance = parseFloat(totalBalance.innerText);
-    const balance = preBalance + IncomeInputValue;
-    const totalAmountBalances = balance - totalAmount;
-    totalBalance.innerText = totalAmountBalances;
-
-  
-        
-    
-    
+  const totalAmount = expensesToatalAmout + foodInputValue + rentInputValue + clothersInputValue;
+  expensesAmount.innerText = totalAmount;
 
 
 
@@ -76,5 +51,49 @@ const button = document.getElementById('calculate-button').addEventListener('cli
 
 
 
+  // income input value
+
+  const IncomeInputValue = getAmount('income-input');
+
+
+
+
+
+  const totalBalance = document.getElementById('total-Balance');
+  const preBalance = parseFloat(totalBalance.innerText);
+  const balance = preBalance + IncomeInputValue;
+  const totalAmountBalances = balance - totalAmount;
+  totalBalance.innerText = totalAmountBalances;
 
 })
+
+
+
+
+  // calculate-click \
+ const calculateButton = document.getElementById('calculate-click').addEventListener('click', function (totalAmountBalances) {
+    // save-input
+    const saveInput = getAmount('save-input')
+
+    const saveAmount = document.getElementById('save-amount');
+    const amount = parseFloat(saveAmount.innerText);
+    const saveBlacne = amount + saveInput;
+    saveAmount.innerText = saveBlacne; 
+    const totalBalance = totalAmountBalances / saveBlacne
+   
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
